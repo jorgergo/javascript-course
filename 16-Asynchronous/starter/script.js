0be793af91b7d6087ad2c1e9eb9b18c418977b67
Your Name <you@example.com>
2024-03-31 02:31:05 -0600

    Refactor AJAX calls to use fetch API

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -3,4 +3,140 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+const renderCountry = function (data, className = '') {
+  const html = `
+  <article class="country ${className}">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+  </article>
+  `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+/*
 ///////////////////////////////////////
+// Our First AJAX Call: XMLHttpRequest
+
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    const html = `
+  <article class="country">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+  </article>
+  `;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryData('portugal');
+getCountryData('usa');
+getCountryData('germany');
+*/
+
+///////////////////////////////////////
+// Welcome to Callback Hell
+
+/*
+const getCountryAndNeighbour = function (country) {
+  // AJAX call country 1
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    // Render country 1
+    renderCountry(data);
+
+    // Get neighbour country (2)
+    const [neighbour] = data.borders;
+
+    if (!neighbour) return;
+
+    // AJAX call country 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.eu/rest/v2/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const data2 = JSON.parse(this.responseText);
+      console.log(data2);
+
+      renderCountry(data2, 'neighbour');
+    });
+  });
+};
+
+// getCountryAndNeighbour('portugal');
+getCountryAndNeighbour('usa');
+
+setTimeout(() => {
+  console.log('1 second passed');
+  setTimeout(() => {
+    console.log('2 seconds passed');
+    setTimeout(() => {
+      console.log('3 second passed');
+      setTimeout(() => {
+        console.log('4 second passed');
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
+
+*/
+
+///////////////////////////////////////
+
+// const request = fetch('https://restcountries.eu/rest/v2/name/portugal');
+
+// console.log(request);
+
+// fetch() returns a promise that needs to be handled with .then() and a callback function, in this case we return .json() to handle the promise. Then, we get another promise with the data in json, which we handle using .then() and a callback function that receives the data as an argument.
+
+const getCountryData = function (country) {
+  // this fetch method returns a promise that needs to be hanled with the then method and json callback function
+  fetch(`https://countries-api-836d.onrender.com/countries/name/${country}`)
+    // This then method returns the country data in json as a promise
+    .then(response => response.json())
+    // This then method uses the data from the json, renders the country and returns a new promise (for the neighbour country) using the data from the main country
+    .then(response => {
+      renderCountry(response[0]);
+
+      const neighbourCountry = response[0].borders?.[0];
+      if (!neighbourCountry) return;
+
+      return fetch(`https://restcountries.com/v2/alpha/${neighbourCountry}`);
+    })
+    .then(response => response.json()) // then we handle the promise and get a new promise with the data in json
+    .then(response => renderCountry(response, 'neighbour')); // finally, we handle the promise that contains our data, and render it
+};
+
+getCountryData('portugal');
